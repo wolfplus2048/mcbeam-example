f4505b635a3d5658c326ab406149728bb6d12540
wolfplus <wolfplus@gmail.com>
2020-09-30 18:25:30 +0800

    add

--- auth_srv/main.go
+++ auth_srv/main.go
@@ -5,16 +5,13 @@ import (
 	"github.com/micro/go-micro/v2/config/cmd"
 	"github.com/micro/go-micro/v2/logger"
 	"github.com/micro/go-micro/v2/registry/etcd"
-	"github.com/micro/go-micro/v2/server"
 	"github.com/micro/go-plugins/store/redis/v2"
 	"github.com/wolfplus2048/mcbeam-example/auth_srv/handler"
 	"github.com/wolfplus2048/mcbeam-plus"
-	"github.com/wolfplus2048/mcbeam-plus/mcb_server/grpc"
 )
 
 func init() {
 	cmd.DefaultStores["redis"] = redis.NewStore
-	server.DefaultServer = grpc.NewServer()
 }
 func main() {
 	logger.Init(logger.WithLevel(logger.DebugLevel))
@@ -27,10 +24,9 @@ func main() {
 	if err := service.Init(); err != nil {
 		logger.Fatal(err)
 	}
-	s := service.Options().Service.Server()
-	s.Handle(s.NewHandler(&handler.Handler{Service: service.Options().Service}))
-	//service.Register(&handler.Handler{Service: service.Options().Service},
-	//	component.WithName("handler"))
+	//s := service.Options().Service.Server()
+	//s.Handle(s.NewHandler(&handler.Handler{Service: service.Options().Service}))
+	service.Register(&handler.Handler{Service: service.Options().Service})
 	if err := service.Run(); err != nil {
 		logger.Fatal(err)
 	}
